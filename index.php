af0e4ec3c07d7b59217da5cf4bd7497777470315
Rohith Asrk <rohith.asrk@gmail.com>
2017-10-14 22:29:50 +0530

    Add basic html

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Paste bin</title>
+  </head>
+  <body>
+    Paste Here:
+    <form action="#" method="post">
+        <textarea rows="8" cols="50" name="paste_data" placeholder="Enter text..."></textarea>
+        <br>
+        <input type="submit"/>
+    </form>
+  </body>
+</html>
